2aab670f9bc99cb27bf5d7a3d3ab8250dec27a34
wajihulhasan01 <wajihulhasan01@gmail.com>
2025-01-15 14:07:47 +0500

    update

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let btn1 = document.querySelector("#head")
-let btn2 = document.querySelector("#tail")
-let result = document.querySelector("#result")
-let img = document.querySelector("#img")
-let num;
-function  toss (coin){
-  num = Math.ceil(Math.random(num)*2)
-  if(coin===1 && num===1 || coin === 2 && num === 2)
-  {
-    result.innerHTML="You win the toss"
-    console.log("you win");
-    
-    
-  }
-  else {
-    result.innerHTML="You loss"
-    console.log("you loss");
-    
-  }
-  if(num===1){
-    img.src ="head.png"
-  }
-  else{
-    img.src="tails.png"
-  }
+const btnHead = document.querySelector("#head");
+const btnTail = document.querySelector("#tail");
+const resultElement = document.querySelector("#result");
+const imgElement = document.querySelector("#img");
+
+const images = {
+  head: "IvgCsVNkTsbtItWdonrGOVaIoU4.png",
+  tail: "t-oHRyHjkwCYqr02mFH1-9pxZpc.png"
+};
+
+function toss(selectedCoin) {
+  resultElement.innerHTML = "";
+  imgElement.style.animation = "none";
+  void imgElement.offsetWidth;
+  imgElement.style.animation = "flip 1s";
+  const randomCoin = Math.ceil(Math.random() * 2);
+  setTimeout(() => {
+    imgElement.src = randomCoin === 1 ? images.head : images.tail;
+    resultElement.innerHTML = selectedCoin === randomCoin
+      ? "You Win the Toss!"
+      : "You Lose the Toss!";
+  }, 1000);
 }
+
+btnHead.addEventListener("click", () => toss(1));
+btnTail.addEventListener("click", () => toss(2));
